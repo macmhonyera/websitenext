776408d370f1e0c6fa975cb716fad043f3f453b5
addyaddy22 <mutamboadmire2@gmail.com>
2024-05-16 02:26:42 +0200

    main website sections added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import Header from "./header/page";
+import Body from "./components/body";
+import Footer from "./footer/page";
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header />
+      <Body />
+      <Footer />
     </main>
   );
 }
