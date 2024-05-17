46b3d050d0b790b19a39284f7cae65feadbcdba7
macmhonyera <macmhonyera@gmail.com>
2024-05-17 11:23:38 +0200

    find talent page designed

--- src/app/find_talent/page.tsx
+++ src/app/find_talent/page.tsx
@@ -1,11 +1,172 @@
-import React from 'react'
+import React from "react";
+import Header from "../header/page";
 
 const FindTalent = () => {
   return (
-      <div>
-          <h1>Find Talent component</h1>
+    <div>
+      <Header />
+      <div className="flex flex-col justify-center items-center p-20">
+        <div>
+          <h3 className="flex text-center justify-center text-md font-medium text-green-700">
+            Jinda Hiring
+          </h3>
+        </div>
+        <div>
+          <h1 className="flex text-center justify-center font-semibold text-3xl font-serif">
+            Hire the best employees in the world!
+          </h1>
+        </div>
+        <div className="flex align-middle justify-between items-center gap-10 p-10">
+          <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+            <h5 className="mb-4 text-xl font-medium text-gray-500">
+              Career Center
+            </h5>
+            <p className="mb-4 text-sm font-medium text-gray-900">
+              Let top creative talent come to you by posting your listing on #1
+              design career center.
+            </p>
+            <div className="flex text-gray-900 flex-col items-start">
+              <div className="flex items-baseline">
+                <span className="text-3xl font-semibold">$</span>
+                <span className="text-5xl font-extrabold tracking-tight ">
+                  49
+                </span>
+                <span className="ms-1 text-xl font-normal text-gray-500 ">
+                  /month
+                </span>
+              </div>
+              <p className="ms-1 text-sm font-normal text-gray-500 ">
+                Billed Monthly
+              </p>
+            </div>
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Average of 1.1k targeted clicks per month
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Easily hire for full-time or freelance placements
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500  ms-3">
+                  Swap out listings as needed
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              className="text-white bg-gray-800 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-full text-md px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Post a Job
+            </button>
+          </div>
+          <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+            <h5 className="mb-4 text-xl font-medium text-gray-500">
+              Hiring Suite
+            </h5>
+            <p className="mb-4 text-sm font-medium text-gray-900">
+              Tap into our ready-to-hire community of top designers with our
+              seamless hiring solution.
+            </p>
+            <div className="flex text-gray-900 flex-col items-start">
+              <div className="flex items-baseline">
+                <span className="text-3xl font-semibold">$</span>
+                <span className="text-5xl font-extrabold tracking-tight ">
+                  149
+                </span>
+                <span className="ms-1 text-xl font-normal text-gray-500 ">
+                  /month
+                </span>
+              </div>
+              <p className="ms-1 text-sm font-normal text-gray-300">
+                billed Monthly
+              </p>
+            </div>
+            <ul role="list" className="space-y-5 my-7">
+              <li className="flex items-center">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Search our entire database of available employees.
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500 ms-3">
+                  Filter by work, location, budget, and more
+                </span>
+              </li>
+              <li className="flex">
+                <svg
+                  className="flex-shrink-0 w-4 h-4 text-green-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                <span className="text-base font-normal leading-tight text-gray-500  ms-3">
+                  Save employees for future needs
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              className="text-black border border-black hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-full text-md px-5 py-2.5 inline-flex justify-center w-full text-center"
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FindTalent
+export default FindTalent;
